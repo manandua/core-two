ac88fd0d1c61b8aec0fa02cc764f3f09d8ef85a9
Manan Dua <53062599+manandua@users.noreply.github.com>
2022-03-08 09:37:29 -0500

    update

--- studio/project-2/js/global.js
+++ studio/project-2/js/global.js
@@ -1,25 +1,73 @@
 const text = document.querySelector('.if');
-window.addEventListener('scroll', () => {
-    const current = window.scrollY;
-    text.style.fontSize = `clamp(1em, 15em - ${current*3.15}px, 15em)`;
+let n = 0;
 
-    if (current >= 64) {
-        text.classList.add('fill')
-        text.style.transform = `translate(-30%,0)`
-
-
-    } else if (current < 64) {
-        if (text.classList.contains('fill'))
-            text.classList.remove('fill')
-        text.style.transform = `translate(0,0)`
-    }
-
-    if (current >= 66) {
-        document.querySelector('.pb').classList.add('pb-visible')
-    } else if (current < 66) {
-        if (document.querySelector('.pb').classList.contains('pb-visible'))
-            document.querySelector('.pb').classList.remove('pb-visible')
-    }
+function next() {
+    n++;
+    scroll()
+}
+let pb = document.querySelector('.pb')
+let sm = document.querySelector('.sm')
 
 
+let ball = [];
+
+for (let i = 0; i < 119; i++) {
+    let n
+    if (i < 10)
+        ball[i] = "images/ball/Comp 1_0000" + i + ".png"
+
+    else if (i < 100)
+        ball[i] = "images/ball/Comp 1_000" + i + ".png"
+
+    else
+        ball[i] = "images/ball/Comp 1_00" + i + ".png"
+}
+const ballImages = ball.map(src => {
+    const img = new Image();
+    img.src = src;
+    return img;
 })
+
+
+
+function scroll() {
+    if (n == 0) {
+        text.classList.remove('fill')
+        text.style.transform = "translateX(0%)"
+
+    }
+    if (n == 1) {
+        text.classList.add('fill')
+        text.style.transform = "translateX(-22%)"
+        setTimeout(() => {
+            pb.classList.add('pb-visible')
+
+        }, 600);
+    } else if (n == 2) {
+        document.querySelector('.sm-wrap').classList.add('sm-visible')
+        sm.classList.add('pb-visible')
+
+    } else if (n == 3) {
+        document.querySelectorAll('.if-wrap').forEach((x) => {
+            x.classList.add('invisible')
+        })
+
+        setTimeout(() => {
+            let i = 0;
+            let myInteval = setInterval(() => {
+                document.querySelector('.img-holder').style.backgroundImage = "url('" + ball[i++] + "')"
+            }, 42);
+            setTimeout(() => {
+                clearInterval(myInteval)
+            }, 4956);
+        }, 1000);
+
+    }
+
+}
+
+function prev() {
+    if (n != 0)
+        n--;
+    scroll();
+}
