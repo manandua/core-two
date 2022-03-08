ea822857ff1b83a3fc44e9a82d06fb7dc043fff7
Manan Dua <53062599+manandua@users.noreply.github.com>
2022-03-08 18:20:18 -0500

    final-mobile
    experience

--- studio/project-2/js/global.js
+++ studio/project-2/js/global.js
@@ -1,6 +1,8 @@
 const text = document.querySelector('.if');
 let n = 0;
 
+
+
 function next() {
     n++;
     scroll()
@@ -34,34 +36,90 @@ function scroll() {
     if (n == 0) {
         text.classList.remove('fill')
         text.style.transform = "translateX(0%)"
-
     }
     if (n == 1) {
         text.classList.add('fill')
-        text.style.transform = "translateX(-22%)"
+        setTimeout(() => {
+            document.querySelector('.if-wrap').style.left = "0"
+        }, 200);
         setTimeout(() => {
             pb.classList.add('pb-visible')
 
-        }, 600);
+        }, 800);
     } else if (n == 2) {
         document.querySelector('.sm-wrap').classList.add('sm-visible')
         sm.classList.add('pb-visible')
 
     } else if (n == 3) {
-        document.querySelectorAll('.if-wrap').forEach((x) => {
-            x.classList.add('invisible')
-        })
+        
+            document.querySelector('.first').classList.add('invisible')
+    
 
         setTimeout(() => {
             let i = 0;
             let myInteval = setInterval(() => {
+                if(i==10)
+                document.body.style.backgroundColor="#0f0f0f"
                 document.querySelector('.img-holder').style.backgroundImage = "url('" + ball[i++] + "')"
             }, 42);
             setTimeout(() => {
                 clearInterval(myInteval)
-            }, 4956);
+          
+            }, 4998);
         }, 1000);
 
+    }else if(n==4){
+        document.querySelector(".tw").classList.remove("hidden");
+        setTimeout(() => {
+            document.querySelector(".tw-1").classList.remove("hidden");
+
+        }, 800);
+    }else if(n==5){
+    
+            let i = 117;
+            let myInteval = setInterval(() => {
+                if(i==10)
+               { document.body.style.backgroundColor="#F7EDE2"
+                document.querySelector('.after-text').classList.add('hidden')
+            }
+                document.querySelector('.img-holder').style.backgroundImage = "url('" + ball[i--] + "')"
+            }, 42);
+            setTimeout(() => {
+
+                clearInterval(myInteval)
+                document.querySelector(".paper-closing").classList.add("invisible");
+                document.querySelector(".tb").classList.remove("hidden")
+            }, 4956);
+        
+    } else if(n==6){
+        document.body.style.backgroundColor="#0F0F0F"
+        document.querySelector('.tb').classList.add('tb-final');
+        setTimeout(() => {
+            setTimeout(() => {
+                document.querySelector('.soo').classList.remove('invisible')
+            }, 500);
+        }, 600);
+    } else if(n==7){
+        document.querySelector('.end1').classList.add('invisible');
+        setTimeout(() => {
+            document.querySelector('.of').classList.remove('invisible');
+        }, 500);
+    }else if(n==8){
+        document.querySelector('.red').classList.add('red-fill')
+        setTimeout(() => {
+            setTimeout(() => {
+                document.querySelector('footer').classList.add('footer-vis');
+            }, 300);
+
+            let html = `<marquee  class="final" direction="left">
+            <h1>it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them &nbsp; &nbsp; &nbsp; it <span style="color: #F7EDE2;">kills</span> them</h1>
+        </marquee>
+        <marquee  class="final final2" direction="right">
+            <h1>it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them  &nbsp; &nbsp; &nbsp;it <span style="color: #0f0f0f;">kills</span> them </h1>
+        </marquee>`;
+        document.body.innerHTML+=html;
+
+        }, 700);
     }
 
 }
